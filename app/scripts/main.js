cca0a18f780879eb01d18c99c2c047eeda348952
Ahmed Abdel-wahab <ahmed.abdel-wahab@robustastudio.com>
2015-11-05 15:45:57 +0200

    inital commit

---
+++ app/scripts/main.js
@@ -0,0 +1,11 @@
+'use strict';
+$(document).ready(function(){
+
+  /* SVG injection in the body
+  $.get('images/svg-defs.svg', function(data) {
+    var $icons = $('<div class="icons"></div>').html(new XMLSerializer().serializeToString(data.documentElement));
+    $('body').prepend($icons);
+  */
+
+  });
+});
