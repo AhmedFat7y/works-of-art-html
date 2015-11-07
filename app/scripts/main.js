24e283c42ce4da323333dd7807c37ef62a1e869c
Ahmed Abdel-wahab <ahmed.abdel-wahab@robustastudio.com>
2015-11-07 14:17:07 +0200

    initalin commit

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,11 +1,8 @@
+/* global Maplace */
 'use strict';
 $(document).ready(function(){
-
-  /* SVG injection in the body
   $.get('images/svg-defs.svg', function(data) {
     var $icons = $('<div class="icons"></div>').html(new XMLSerializer().serializeToString(data.documentElement));
     $('body').prepend($icons);
-  */
-
   });
 });
