a6549554f4c784a25ebab24db62008bed30521e0
Ahmed Abdel-wahab <ahmed.abdel-wahab@robustastudio.com>
2015-11-14 00:56:23 +0200

    sliders kinda working now. Still need more work

--- app/scripts/functions.js
+++ app/scripts/functions.js
@@ -12,32 +12,75 @@ function getData() {
       id:2, 
       ch:2, 
       pid:1, 
-      title: 'hello world-1'
+      title: 'hello world-2'
     },
     {
       id:3, 
       ch:3, 
       pid:2, 
-      title: 'hello world-1'
+      title: 'hello world-3'
     },
     {
       id:4, 
       ch:3, 
       pid:2, 
-      title: 'hello world-1'
+      title: 'hello world-4'
     },
     {
       id:5, 
       ch:2, 
       pid:1, 
-      title: 'hello world-1'
+      title: 'hello world-5'
     },
     {
       id:6,
       ch:3,
       pid:5, 
-      title: 'hello world-1'
+      title: 'hello world-6'
     },
   ];
+}
+
+function getChapters(data, chapterNumber, parentId) {
+  return data.filter(function(item){
+    return ((parentId === 0 || item.pid === parentId) && item.ch === chapterNumber);
+  });
+}
+
+function getMaxChapter(data) {
+  return data.reduce(function(a, b){
+    return a.ch >= b.ch? a : b;
+  }, {ch:0});
+}
+
+function _buildTreeLevels(data) {
+  
+  var max = getMaxChapter(data).ch;
+  var resultTree = new Array(max);
+  for (var i = 0; i < max; i++) {
+    resultTree[i] = getChapters(data, i + 1, 0);
+  }
+  return resultTree;
+}
+
+function chapterToSlide(chapter){
+  return '<div class="slide" id="' + chapter.id +'" data-parent="' + chapter.pid + '">'
+        +'<div class="slide-content">'
+        +'<h4>' + chapter.title + '</h4>'
+        +'</div>'
+        +'</div>';
+}
+
+function buildChapterSlides() {
+  var data = getData();
+  var chaptersTree = _buildTreeLevels(data);
+  return chaptersTree.map(function(level) {
+    return level.map(function(chapter) {
+      chapter.html = chapterToSlide(chapter);
+      return chapter;
+    });
+  });
 
 }
+
+var chaptersTree = buildChapterSlides();
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,4 @@
-/* global Maplace */
+/* global Maplace chaptersTree */
 'use strict';
 $(document).ready(function(){
   // $.get('images/svg-defs.svg', function(data) {
@@ -14,23 +14,47 @@ $(document).ready(function(){
       },
       theme: 'snow',
     });
-  }
+  }//endif
   
-  $('.slider-for').slick({
+  $('.slider-1, .slider-2, .slider-3').slick({
     slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: true,
-    fade: true,
-    asNavFor: '.slider-nav'
-  });
-
-  $('.slider-nav').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
+    // slidesToScroll: 1,
     centerPadding: '10px',
     centerMode: true,
-    focusOnSelect: true
+    adaptiveHeight: true,
+    // asNavFor: '.slider-3',
+    // focusOnSelect: true
   });
+  
+  $(chaptersTree).each(function(index, treeLevel) {
+    var slider = $('.slider-' + (index+1));
+    $(treeLevel).each(function(index2, chapter){
+      slider.slick('slickAdd', chapter.html);
+    });
+  });
+  
+  $('.slider-1, .slider-2, .slider-3').slick('slickGoTo', 0);
+
+  $('.slider-2').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+    var id = slick.$slides[nextSlide].id;
+    $('.slider-3').slick('slickFilter', '[data-parent=' + id + ']');
+  });
+  // $('').slick({
+  //   slidesToShow: 3,
+  //   slidesToScroll: 1,
+  //   // asNavFor: '.slider-3',
+  //   centerPadding: '10px',
+  //   centerMode: true,
+  //   focusOnSelect: true
+  // });
+
+  // $('').slick({
+  //   slidesToShow: 3,
+  //   slidesToScroll: 1,
+  //   // asNavFor: '.slider-2',
+  //   centerPadding: '10px',
+  //   centerMode: true,
+  //   focusOnSelect: true
+  // });
 
 });
